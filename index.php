99fd190f739bbb6e97b2d0cb6efd8fefa34b97d0
Matizin46 <mattheus.paes.br@gmail.com>
2025-06-27 22:25:46 -0300

    ???

--- index.php
+++ index.php
@@ -1,26 +1,41 @@
 <?php include 'db.php'; ?>
+
 <!DOCTYPE html>
 <html lang="pt-br">
 <head>
   <meta charset="UTF-8">
-  <title>ChampionsLeague - Início</title>
+  <title>Papelaria Criativa</title>
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
-  <link rel="stylesheet" href="css/estilo.css">
 </head>
+ 
 <body>
+ 
   <div class="container mt-4">
-    <h1 class="text-center text-primary">Estrelas da Champions League</h1>
-    <div id="carouselExample" class="carousel slide" data-bs-ride="carousel">
+    <h1 class="text-center mb-4">Papelaria Criativa</h1>
+ 
+ 
+    <div id="carouselExample" class="carousel slide mb-5" data-bs-ride="carousel">
       <div class="carousel-inner">
-        <div class="carousel-item active"><img src="img/messi.jpg" class="d-block w-100" alt="Messi"></div>
-        <div class="carousel-item"><img src="img/ronaldo.jpg" class="d-block w-100" alt="Ronaldo"></div>
-        <div class="carousel-item"><img src="img/mbappe.jpg" class="d-block w-100" alt="Mbappé"></div>
+        <div class="carousel-item active">
+          <img src="https://akm-img-a-in.tosshub.com/indiatoday/images/story/202207/ronaldounitedatleticotransfer_1200x768.png?VersionId=D6fapmJ8W5Qj6z1vpqZusEu1gL.H_79l" class="d-block w-100" alt="Banner 1">
+        </div>
+        <div class="carousel-item">
+          <img src="https://assets.goal.com/images/v3/bltec70bd945d5375a2/GOAL%20-%20Blank%20WEB%20-%20Facebook%20(2).jpg?auto=webp&format=pjpg&width=3840&quality=60">
+        </div>
+        <div class="carousel-item">
+          <img src="https://e0.365dm.com/22/12/2048x1152/skysports-lionel-messi-argentina_5997509.png?20221215150028" class="d-block w-100" alt="Banner 3">
+        </div>
       </div>
+      <button class="carousel-control-prev" type="button" data-bs-target="#carouselExample" data-bs-slide="prev">
+        <span class="carousel-control-prev-icon"></span>
+      </button>
+      <button class="carousel-control-next" type="button" data-bs-target="#carouselExample" data-bs-slide="next">
+        <span class="carousel-control-next-icon"></span>
+      </button>
     </div>
-    <div class="text-center mt-4">
-      <a href="listagem.php" class="btn btn-success">Ver Jogadores</a>
-    </div>
-  </div>
-  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>
-</body>
+ 
+
+ 
+  </body>
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"></script>
 </html>
